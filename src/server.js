763a5b2698f291dd5d1ff595586651c75126a8c4
Riad Hossen <riad.hossen.2009@gmail.com>
2025-02-16 14:46:28 -0600

    Port Change

--- src/server.js
+++ src/server.js
@@ -76,7 +76,7 @@ app.get('/result', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 }).on('error', (err) => {
